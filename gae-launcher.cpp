b36e0a37d0b13771aaad0bb9a45f0fdbe6e252f3
Hardik-7892 <149086108+Hardik-7892@users.noreply.github.com>
2024-02-09 00:42:21 +0530

    Create gae-launcher.cpp

---
+++ gae-launcher.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hardik Pandey"<<endl;
+    cout<<"21BCS7892"<<endl;
+    cout<<"21BCS-FL-604-A"<<endl;
+    cout<<"mark = 28/30?"<<endl;
+    return 0;
+}
